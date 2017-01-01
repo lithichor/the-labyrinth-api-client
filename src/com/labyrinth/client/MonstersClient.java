ece8d4aecd2a93693bbb8495a567d95102f49126
Eric Hartill <euclidsieve@gmail.com>
2017-01-01 09:09:37 -0500

    added monsters client

---
+++ src/com/labyrinth/client/MonstersClient.java
@@ -0,0 +1,48 @@
+package com.labyrinth.client;
+
+public class MonstersClient extends LabyrinthApiClient
+{
+	/**
+	 * The constructor using localhost as the server url
+	 * 
+	 * @param username - email address for the authenticated user
+	 * @param password - password for the authenticated user
+	 */
+	public MonstersClient(String username, String password)
+	{
+		super(username, password);
+	}
+	
+	/**
+	 * A more general constructor for arbitrary server urls
+	 * 
+	 * @param url - the url for the server
+	 * @param username - email address for the authenticated user
+	 * @param password - password for the authenticated user
+	 */
+	public MonstersClient(String url, String username, String password)
+	{
+		super(url, username, password);
+	}
+	
+	/**
+	 * get a Monster using the monsterId as an Integer
+	 * @param monsterId
+	 * @return
+	 */
+	public String getMonster(Integer monsterId)
+	{
+		return getResponse(makeGetMethod("monsters/" + monsterId));
+	}
+
+	/**
+	 * get a Monster using the monsterId as a String
+	 * @param monsterId
+	 * @return
+	 */
+	public String getMonster(String monsterId)
+	{
+		return getResponse(makeGetMethod("monsters/" + monsterId));
+	}
+
+}
