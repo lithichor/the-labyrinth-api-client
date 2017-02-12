0a03772a239cbc0e8026fae1a51409b4abd06606
Eric Hartill <euclidsieve@gmail.com>
2017-02-12 13:46:52 -0500

    #27 - added Options method and created options for user - added javadoc for turns

--- src/com/labyrinth/client/TurnsClient.java
+++ src/com/labyrinth/client/TurnsClient.java
@@ -2,31 +2,62 @@ package com.labyrinth.client;
 
 public class TurnsClient extends LabyrinthApiClient
 {
+	/**
+	 * General constructor for when server is not local
+	 * @param url
+	 * @param username
+	 * @param password
+	 */
 	public TurnsClient(String url, String username, String password)
 	{
 		super(url, username, password);
 	}
 
+	/**
+	 * Constructor for localhost servers
+	 * @param username
+	 * @param password
+	 */
 	public TurnsClient(String username, String password)
 	{
 		super(username, password);
 	}
 
+	/**
+	 * Get a Turn using an Integer ID
+	 * @param turnId
+	 * @return
+	 */
 	public String getTurn(Integer turnId)
 	{
 		return getResponse(makeGetMethod("turns/" + turnId));
 	}
 
+	/**
+	 * Get a Turn using a String ID
+	 * @param turnId
+	 * @return
+	 */
 	public String getTurn(String turnId)
 	{
 		return getResponse(makeGetMethod("turns/" + turnId));
 	}
 
+	/**
+	 * Get the current Turn for a Game using an Integer GameId
+	 * @param gameId
+	 * @return
+	 */
 	public String getTurnForGame(Integer gameId)
 	{
 		return getResponse(makeGetMethod("turns/game/" + gameId));
 	}
 
+	/**
+	 * Get the current Turn for a Game using a String GameId
+	 * @param gameId
+	 * @return
+	 */
 	public String getTurnForGame(String gameId)
 	{
 		return getResponse(makeGetMethod("turns/game/" + gameId));
